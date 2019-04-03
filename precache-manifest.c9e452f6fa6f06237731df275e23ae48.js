fd56282967121729b19f97ed9751075e7071e752
Ameerahalshihry <Ameerahalshihry@gmail.com>
2019-04-03 16:30:47 +0300

    Updates

---
+++ precache-manifest.c9e452f6fa6f06237731df275e23ae48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e5fe848f7fb2d7b335160a36c3aa69e",
+    "url": "/project_2/static/media/img15.3e5fe848.jpg"
+  },
+  {
+    "revision": "d3ff07a08b1c432861a6ee37241ca041",
+    "url": "/project_2/static/media/img14.d3ff07a0.png"
+  },
+  {
+    "revision": "a1b30321f5fb7a1d2f25",
+    "url": "/project_2/static/js/runtime~main.cf167350.js"
+  },
+  {
+    "revision": "ae816bf1fecaecfe920e",
+    "url": "/project_2/static/js/main.3788deaf.chunk.js"
+  },
+  {
+    "revision": "1c527b467090b4b5d506",
+    "url": "/project_2/static/js/2.f60c14ad.chunk.js"
+  },
+  {
+    "revision": "ae816bf1fecaecfe920e",
+    "url": "/project_2/static/css/main.efdf30a4.chunk.css"
+  },
+  {
+    "revision": "a3d3102c69857d1c9dab4e255aa85707",
+    "url": "/project_2/index.html"
+  }
+];
